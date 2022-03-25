5ae1c3f567d52ed2926fd95356fefe90dbdcfbc2
davisstanko <davisbvstanko@gmail.com>
2022-03-25 13:59:15 -0400

    final

--- README.md
+++ README.md
@@ -20,23 +20,23 @@ Start the program by executing the main.py file.
 
 Runs `time.time()` before and after the sorting algorithm has run and returns the difference. `time.time()` returns the time in seconds since the epoch as a floating point number. Learn more [here](https://docs.python.org/3/library/time.html#time.time).
 
-### other
+### time.perf_counter()
 
-The other method
+Runs `time.perf_counter()` before and after the sorting algorithm has run and returns the difference. `time.perf_counter()` has no relation to 'human' time and should only be used to mesure the difference in time between two points. Learn more [here](https://docs.python.org/3/library/time.html#time.perf_counter).
 
 ## Sorting Methods
 
-### Bogo Sort
+### Bogo Sort --> Fun
 
 Randomly sorts the given data untill it is sorted correctly. This means with luck it could be successful the first attempt. On the other hand, there is a small chance that the list continuesly gets sorted in an incorrect way and will take infinitly long. It is reccomened that you use a small data set to maximize the chance of the sorting algorithm suceeding quickly. Max number has no effect as bogo sort does not consider it.
 
 My implementation of bogo sort is purely for fun and should not be used for any real world applications. To check whether the random sort worked, I used pythons built-in function to sort the list and compare it with the bogo results. However, this means that before the timer even starts for bogo sort, the list has already been sorted! Therefore, even in the best case scenario using my implementation, bogo sort is a waste of time.
 
-### Radix Sort
+### Radix Sort --> Speed
 
-Theoretically the fastest sorting algorithm in this program. It works by grouping elements by their single digit place values and then ordering them. In this program, it uses counting sort as the intermidiate sorting algorithm. Max number and data set size both have an effect on this algorithm but it should not struggle with any reasonable data set. However, due to the nature of radix sort, it can be very memory intensive in a worst case scenario due to the temporary storage of elements sorted by single digits.  
+Theoretically the fastest sorting algorithm in this program. It works by grouping elements by their single digit place values and then ordering them. In this program, it uses counting sort as the intermidiate sorting algorithm. Max number and data set size both have an effect on this algorithm but it should not struggle with any reasonable data set. However, due to the nature of radix sort, it can be very memory intensive in a worst case scenario due to the temporary storage of elements sorted by single digits. 
 
-### Numpy Sort
+### Numpy Sort --> Lightweight
 
 Built into python and is sufficent for most use cases. It attempt to provide the best of both speed and memory useage. The default sorting mechanism is quicksort but this can be changed with arguments. This program uses quicksort. This should be able to sort any reasonable data set but will take longer than radix sort on average.
 
--- main.py
+++ main.py
@@ -18,7 +18,7 @@ fatal_error = "Fatal error: Please resart the program. If the error persists, pl
 
 print("\n" * 100)  # Clear screen
 
-################################################## Sorting Algorithms ##################################################
+############################################ time.time() Sorting Algorithms ############################################
 
 
 def time_bogo_sort(bogo_data_set, sorted_data_set):  # Bogo sort timed with time.time()
@@ -81,15 +81,86 @@ def time_radix_sort(radix_data_set):
     print(f"\nTime taken: {difference} seconds")
     total_time = total_time + difference
 
-# Numpy sort timed with time.time()
 
-
-def time_numpy_sort(numpy_data_set):  # Default numpy sort no arguments
+def time_numpy_sort(numpy_data_set):  # Default numpy sort no arguments timed with time.time()
     global total_time
     temp_data_set = numpy_data_set.copy()
     start = time.time()
     numpy_sorted_data_set = np.sort(temp_data_set)
     end = time.time()
+    difference = end - start
+    print(f"\nNumpy sorted data set:\n{numpy_sorted_data_set}")
+    print(f"\nTime taken: {difference} seconds\n")
+    total_time = total_time + difference
+
+######################################## time.perf_counter() Sorting Algorithms ########################################
+
+
+def perf_bogo_sort(bogo_data_set, sorted_data_set):  # Bogo sort timed with time.perf_counter()
+    global total_time
+    temp_data_set = bogo_data_set.copy()
+    start = time.perf_counter()
+    while temp_data_set != sorted_data_set:
+        random.shuffle(temp_data_set)
+    end = time.perf_counter()
+    difference = end - start
+    print(f"\nBogo sorted data set:\n{temp_data_set}")
+    print(f"\nTime taken: {difference} seconds")
+    total_time = total_time + difference
+
+
+# Radix sort timed with time.perf_counter()
+def countingSort(temp_data_set, place):  # Using counting sort to sort the elements in the basis of significant places
+    size = len(temp_data_set)
+    output = [0] * size
+    count = [0] * 10
+
+    # Calculate count of elements
+    for i in range(0, size):
+        index = temp_data_set[i] // place
+        count[index % 10] += 1
+
+    # Calculate cumulative count
+    for i in range(1, 10):
+        count[i] += count[i - 1]
+
+    # Place the elements in sorted order
+    i = size - 1
+    while i >= 0:
+        index = temp_data_set[i] // place
+        output[count[index % 10] - 1] = temp_data_set[i]
+        count[index % 10] -= 1
+        i -= 1
+
+    for i in range(0, size):
+        temp_data_set[i] = output[i]
+
+
+# Main function to implement radix sort
+def perf_radix_sort(radix_data_set):
+    global total_time
+    temp_data_set = radix_data_set.copy()
+    start = time.perf_counter()
+    max_element = max(temp_data_set)  # Get maximum element
+
+    # Apply counting sort to sort elements based on place value.
+    place = 1
+    while max_element // place > 0:
+        countingSort(temp_data_set, place)
+        place *= 10
+    end = time.perf_counter()
+    difference = end - start
+    print(f"Radix sorted data set:\n{temp_data_set}")
+    print(f"\nTime taken: {difference} seconds")
+    total_time = total_time + difference
+
+
+def perf_numpy_sort(numpy_data_set):  # Default numpy sort no arguments timed with time.perf_counter()
+    global total_time
+    temp_data_set = numpy_data_set.copy()
+    start = time.perf_counter()
+    numpy_sorted_data_set = np.sort(temp_data_set)
+    end = time.perf_counter()
     difference = end - start
     print(f"\nNumpy sorted data set:\n{numpy_sorted_data_set}")
     print(f"\nTime taken: {difference} seconds\n")
@@ -124,7 +195,7 @@ def set_trials():
 
 def set_time_choice():
     global time_choice
-    time_choice = int(input("\nPlease select a timing method:\n1. Timing with time.time() (Recommended)\n2. Timing with other\n"))  # Get timing method
+    time_choice = int(input("\nPlease select a timing method:\n1. Timing with time.time()\n2. Timing with time.perf_counter (Recommended)\n"))  # Get timing method
     if time_choice == 1 or time_choice == 2:  # Check if input is valid
         return
     else:
@@ -163,7 +234,6 @@ def main():
             for i in range(int(trials)):
                 time_bogo_sort(data_set, sorted_data_set)
             print(f"\nAverage time taken: {float(total_time)/float(trials)} seconds")
-
         elif sort_choice == 2:
             for i in range(int(trials)):
                 time_radix_sort(data_set)
@@ -176,11 +246,18 @@ def main():
             print(fatal_error)
     elif time_choice == 2:
         if sort_choice == 1:
-            print("Not implemented yet")
+            sorted_data_set = sorted(data_set.copy())  # This is really stupid. It sorts the data set to see if it sorted correctly. Therefore it's quicker to just user sorted() by itself. I have left it out of the timing method to remove it's impact from the overall time.
+            for i in range(int(trials)):
+                perf_bogo_sort(data_set, sorted_data_set)
+            print(f"\nAverage time taken: {float(total_time)/float(trials)} seconds")
         if sort_choice == 2:
-            print("Not implemented yet")
+            for i in range(int(trials)):
+                perf_radix_sort(data_set)
+            print(f"\nAverage time taken: {float(total_time)/float(trials)} seconds")
         if sort_choice == 3:
-            print("Not implemented yet")
+            for i in range(int(trials)):
+                perf_numpy_sort(data_set)
+            print(f"\nAverage time taken: {float(total_time)/float(trials)} seconds")
 
 
 main()
